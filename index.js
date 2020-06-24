068bd8c17f0a67ea57e2aeb83cc9f35f4ba45b2d
Kirill Novik <kino6052@colorado.edu>
2020-06-23 17:53:37 -0700

    Add Debugging Messages

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 9966 });
 
+console.warn("WebSocket Server Started");
+
 wss.on('connection', function connection(ws) {
+  console.warn('connection!');
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
