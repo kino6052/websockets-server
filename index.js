0b4ae5e44ebd5e5a0d8fcefe4ffe38a67f90fb56
Kirill Novik <kino6052@colorado.edu>
2020-06-23 18:18:34 -0700

    Add Handling Logic

--- index.js
+++ index.js
@@ -4,11 +4,32 @@ const wss = new WebSocket.Server({ port: 9966 });
 
 console.warn("WebSocket Server Started");
 
+const sendTexture = (ws, message) => {
+  try {
+    const j = JSON.parse(message);
+    ws.send(j.texture);
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const getIsTexture = (message) => {
+  try {
+    const j = JSON.parse(message);
+    if (j.type === 'texture' && !!j.texture) return true;
+    return false;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
 wss.on('connection', function connection(ws) {
   console.warn('connection!');
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    if(getIsTexture(message)) {
+      sendTexture(ws, message);
+    };
   });
-
-  ws.send('something');
 });
